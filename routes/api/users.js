0cccbed39e4244b197552b85943bef6f3abda0e9
kyo8300 <kametyou777@gmail.com>
2019-10-02 02:00:15 +0800

    connected to DB and added user authenticated

---
+++ routes/api/users.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const { forwardAuthenticated } = require('../../config/auth');
+
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+// @route  GET users
+// @desc   Test route
+// @access Public
+router.get('/', (req, res) => res.send('User route'));
+
+// @route  POST users/register
+// @desc   Register user
+// @access Public
+router.post(
+  '/register',
+  [
+    forwardAuthenticated,
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Email is invalid').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password, password2 } = req.body;
+    try {
+      if (password != password2) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Password not matched.' }] });
+      }
+
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+// @route  GET users/login
+// @desc   Logout user
+// @access private
+router.post('/login', forwardAuthenticated, async (req, res, next) => {
+  try {
+    passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/users/login',
+      failureFlash: true
+    })(req, res, next);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route  GET users/logout
+// @desc   Logout user
+// @access private
+router.get('/logout', async (req, res) => {
+  try {
+    req.logout();
+    res.redirect('/');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+const passport = require('passport');
+const session = require('express-session');
 
+//Passport Config
+require('./config/passport')(passport);
+
+// Cennect Database
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Routes
 app.get('/', (req, res) => res.send('Server Running'));
+app.use('/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
