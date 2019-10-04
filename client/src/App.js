217f5f32fdb55a0d7b17035a20088f7e789b1254
kyo8300 <kametyou777@gmail.com>
2019-10-04 14:51:11 +0800

    Connected to React

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <h1>App</h1>
+    </Fragment>
+  </Router>
+);
+
+export default App;
