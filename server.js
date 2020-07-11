999bb162bb5b4ef11dd62e8d10b50c0cc50b4b80
unknown <kametyou777@gmail.com>
2020-07-11 19:12:56 +0800

    deploy app

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
-const mockData = require('./config/mockdata');
+const connectDB = require("./config/db");
+// const mockData = require('./config/mockdata');
+const path = require("path");
 
 // Cennect Database
 connectDB();
@@ -12,10 +13,20 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 //Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/communities', require('./routes/api/communities'));
-app.use('/api/profiles', require('./routes/api/profiles'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/communities", require("./routes/api/communities"));
+app.use("/api/profiles", require("./routes/api/profiles"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
